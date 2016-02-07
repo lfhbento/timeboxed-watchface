a2751123bdb3c975d6b4ada164dbf603854388cb
Luis Felipe Hussin Bento <hussin@gmail.com>
2016-02-06 17:11:26 -0800

    weather functional, using weather underground for now

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,111 @@
 Pebble.addEventListener("ready",
     function(e) {
         console.log("Hello world! - Sent from your javascript application.");
+	getWeather();
     }
 );
+
+Pebble.addEventListener('appmessage',
+    function(e) {
+        console.log('AppMessage received!');
+        getWeather();
+    }                     
+);
+
+function locationSuccess(pos) {
+    var url = 'http://api.wunderground.com/api/7456558e28f97728/conditions/forecast/q/' + pos.coords.latitude + ',' + pos.coords.longitude + '.json';
+    xhrRequest(url, 'GET', function(responseText) {
+        try {
+            var resp = JSON.parse(responseText);
+            var temp = Math.round(resp.current_observation.temp_c);
+            var max = Math.round(resp.forecast.simpleforecast.forecastday[0].high.celsius);
+            var min = Math.round(resp.forecast.simpleforecast.forecastday[0].low.celsius);
+            var condition = wu_iconToId[resp.forecast.simpleforecast.forecastday[0].icon];
+            if (typeof(condition) === 'undefined') {
+                condition = 0;
+            }
+
+            var data = {
+                'KEY_TEMP': temp,
+                'KEY_MAX': max,
+                'KEY_MIN': min,
+                'KEY_WEATHER': condition
+            }
+
+            Pebble.sendAppMessage(data,
+                function(e) {
+                    console.log('Weather info sent to Pebble successfully!');
+                },
+                function(e) {
+                    console.log('Error sending weather info to Pebble!');
+                }
+            );
+
+        } catch(ex) {
+            console.log(ex);
+        }
+    });
+}
+
+function locationError(err) {
+    console.log('Error requesting location!');
+}
+
+function getWeather() {
+    navigator.geolocation.getCurrentPosition(
+	locationSuccess,
+	locationError,
+	{timeout: 15000, maximumAge: 60000}
+    );
+}
+
+
+var xhrRequest = function (url, type, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function () {
+        callback(this.responseText);
+    };
+    xhr.open(type, url);
+    xhr.send();
+};
+
+var wu_iconToId = {
+    'unknown': 0,
+    'clear': 1,
+    'sunny': 2,
+    'partlycloudy': 3,
+    'mostlycloudy': 4,
+    'mostlysunny': 5,
+    'partlysunny': 6,
+    'cloudy': 7,
+    'rain': 8,
+    'snow': 9,
+    'tstorms': 10,
+    'sleat': 11,
+    'flurries': 12,
+    'hazy': 13,
+    'chancetstorms': 14,
+    'chancesnow': 15,
+    'chancesleat': 16,
+    'chancerain': 17,
+    'chanceflurries': 18,
+    'nt_unknown': 19,
+    'nt_clear': 20,
+    'nt_sunny': 21,
+    'nt_partlycloudy': 22,
+    'nt_mostlycloudy': 23,
+    'nt_mostlysunny': 24,
+    'nt_partlysunny': 25,
+    'nt_cloudy': 26,
+    'nt_rain': 27,
+    'nt_snow': 28,
+    'nt_tstorms': 29,
+    'nt_sleat': 30,
+    'nt_flurries': 31,
+    'nt_hazy': 32,
+    'nt_chancetstorms': 33,
+    'nt_chancesnow': 34,
+    'nt_chancesleat': 35,
+    'nt_chancerain': 36,
+    'nt_chanceflurries': 37,
+};
--- src/timeboxed.c
+++ src/timeboxed.c
@@ -1,6 +1,11 @@
 #include <pebble.h>
 #include <ctype.h>
 
+#define KEY_TEMP 0
+#define KEY_MAX 1
+#define KEY_MIN 2
+#define KEY_WEATHER 3
+
 static Window *watchface;
 static TextLayer *hours;
 static TextLayer *date;
@@ -22,6 +27,10 @@ static GFont weather_font_big;
 
 static char hour_text[8];
 static char date_text[13];
+static char temp_text[8];
+static char max_text[8];
+static char min_text[8];
+static char weather_text[4];
 static char tz_text[20];
 static char steps_or_sleep_text[16];
 static char dist_or_deep_text[16];
@@ -30,13 +39,55 @@ static bool has_health;
 static int tz_hour;
 static int tz_minute;
 static char tz_name[TZ_LEN];
+static char* weather_conditions[] = {
+    "\U0000F07B", // 'unknown': 0,
+    "\U0000F00D", // 'clear': 1,
+    "\U0000F00D", // 'sunny': 2,
+    "\U0000F002", // 'partlycloudy': 3,
+    "\U0000F002", // 'mostlycloudy': 4,
+    "\U0000F00C", // 'mostlysunny': 5,
+    "\U0000F002", // 'partlysunny': 6,
+    "\U0000F013", // 'cloudy': 7,
+    "\U0000F019", // 'rain': 8,
+    "\U0000F01B", // 'snow': 9,
+    "\U0000F01D", // 'tstorms': 10,
+    "\U0000F0b5", // 'sleat': 11,
+    "\U0000F00A", // 'flurries': 12,
+    "\U0000F0b6", // 'hazy': 13,
+    "\U0000F01D", // 'chancetstorms': 14,
+    "\U0000F01B", // 'chancesnow': 15,
+    "\U0000F0b5", // 'chancesleat': 16,
+    "\U0000F008", // 'chancerain': 17,
+    "\U0000F01B", // 'chanceflurries': 18,
+    "\U0000F07B", // 'nt_unknown': 19,
+    "\U0000F02E", // 'nt_clear': 20,
+    "\U0000F02E", // 'nt_sunny': 21,
+    "\U0000F086", // 'nt_partlycloudy': 22,
+    "\U0000F086", // 'nt_mostlycloudy': 23,
+    "\U0000F081", // 'nt_mostlysunny': 24,
+    "\U0000F086", // 'nt_partlysunny': 25,
+    "\U0000F013", // 'nt_cloudy': 26,
+    "\U0000F019", // 'nt_rain': 27,
+    "\U0000F01B", // 'nt_snow': 28,
+    "\U0000F01D", // 'nt_tstorms': 29,
+    "\U0000F0b5", // 'nt_sleat': 30,
+    "\U0000F038", // 'nt_flurries': 31,
+    "\U0000F04A", // 'nt_hazy': 32,
+    "\U0000F01D", // 'nt_chancetstorms': 33,
+    "\U0000F038", // 'nt_chancesnow': 34,
+    "\U0000F0B3", // 'nt_chancesleat': 35,
+    "\U0000F036", // 'nt_chancerain': 36,
+    "\U0000F038", // 'nt_chanceflurries': 37,
+};
+
 
 static void update_time() {
-    // Get a tm structure
+    // Get a tm structuiire
     time_t temp = time(NULL);
     struct tm *tick_time = localtime(&temp);
     struct tm *gmt_time = gmtime(&temp);
     gmt_time->tm_hour = gmt_time->tm_hour + tz_hour;
+    mktime(gmt_time);
     gmt_time->tm_min = gmt_time->tm_min + tz_minute;
     mktime(gmt_time);
 
@@ -73,10 +124,42 @@ static void update_time() {
     text_layer_set_text(alt_time, tz_text);
 }
 
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+    Tuple *temp_tuple = dict_find(iterator, KEY_TEMP);
+    Tuple *max_tuple = dict_find(iterator, KEY_MAX);
+    Tuple *min_tuple = dict_find(iterator, KEY_MIN);
+    Tuple *weather_tuple = dict_find(iterator, KEY_WEATHER);
+
+    // If all data is available, use it
+    if(temp_tuple && max_tuple && min_tuple && weather_tuple) {
+        snprintf(temp_text, sizeof(temp_text), "%dc", (int)temp_tuple->value->int32);
+        snprintf(max_text, sizeof(max_text), "%d", (int)max_tuple->value->int32);
+        snprintf(min_text, sizeof(min_text), "%d", (int)min_tuple->value->int32);
+        snprintf(weather_text, sizeof(weather_text), "%s", weather_conditions[(int)weather_tuple->value->int32]);
+
+        text_layer_set_text(temp_cur, temp_text);
+        text_layer_set_text(temp_max, max_text);
+        text_layer_set_text(temp_min, min_text);
+        text_layer_set_text(weather, weather_text);
+    }
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+}
+
+static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+}
+
+static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
 static void battery_handler(BatteryChargeState charge_state) {
 
     if (charge_state.is_charging) {
-        snprintf(s_battery_buffer, sizeof(s_battery_buffer), "(=)");
+        snprintf(s_battery_buffer, sizeof(s_battery_buffer), "(=/=)");
     } else {
         snprintf(s_battery_buffer, sizeof(s_battery_buffer), (charge_state.charge_percent <= 20 ? "! %d%%" : "%d%%"), charge_state.charge_percent);
     }
@@ -109,7 +192,7 @@ static void watchface_load(Window *window) {
     text_layer_set_text_color(date, GColorWhite);
     text_layer_set_text_alignment(date, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentRight));
     text_layer_set_font(date, date_font);
-    text_layer_set_text(date, "fri.05.feb");
+    text_layer_set_text(date, "");
     
     alt_time = text_layer_create(
         GRect(PBL_IF_ROUND_ELSE(0, -2), PBL_IF_ROUND_ELSE(48, 40), bounds.size.w, 50));
@@ -117,7 +200,7 @@ static void watchface_load(Window *window) {
     text_layer_set_text_color(alt_time, GColorWhite);
     text_layer_set_text_alignment(alt_time, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentRight));
     text_layer_set_font(alt_time, base_font);
-    text_layer_set_text(alt_time, "15:54+1.cst");
+    text_layer_set_text(alt_time, "");
 
     battery = text_layer_create(
         GRect(PBL_IF_ROUND_ELSE(0, -4), PBL_IF_ROUND_ELSE(122, 114), bounds.size.w, 50));
@@ -125,7 +208,7 @@ static void watchface_load(Window *window) {
     text_layer_set_text_color(battery, GColorWhite);
     text_layer_set_text_alignment(battery, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentRight));
     text_layer_set_font(battery, base_font);
-    text_layer_set_text(battery, "70%");
+    text_layer_set_text(battery, "");
 
     weather = text_layer_create(
         GRect(PBL_IF_ROUND_ELSE(-14, 4), PBL_IF_ROUND_ELSE(2, 0), bounds.size.w, 50));
@@ -133,7 +216,7 @@ static void watchface_load(Window *window) {
     text_layer_set_text_color(weather, GColorWhite);
     text_layer_set_text_alignment(weather, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
     text_layer_set_font(weather, weather_font);
-    text_layer_set_text(weather, "\U0000F002");
+    text_layer_set_text(weather, "");
 
     temp_cur = text_layer_create(
         GRect(PBL_IF_ROUND_ELSE(14, 36), PBL_IF_ROUND_ELSE(4, 2), bounds.size.w, 50));
@@ -141,18 +224,18 @@ static void watchface_load(Window *window) {
     text_layer_set_text_color(temp_cur, GColorWhite);
     text_layer_set_text_alignment(temp_cur, PBL_IF_ROUND_ELSE(GTextAlignmentCenter, GTextAlignmentLeft));
     text_layer_set_font(temp_cur, base_font);
-    text_layer_set_text(temp_cur, "-14c");
+    text_layer_set_text(temp_cur, "");
 
     temp_min = text_layer_create(
-        GRect(PBL_IF_ROUND_ELSE(70, 85), PBL_IF_ROUND_ELSE(20, 2), bounds.size.w, 50));
+        GRect(PBL_IF_ROUND_ELSE(70, 85), PBL_IF_ROUND_ELSE(22, 2), bounds.size.w, 50));
     text_layer_set_background_color(temp_min, GColorClear);
     text_layer_set_text_color(temp_min, GColorWhite);
     text_layer_set_text_alignment(temp_min, GTextAlignmentLeft);
     text_layer_set_font(temp_min, base_font);
-    text_layer_set_text(temp_min, "-8");
+    text_layer_set_text(temp_min, "");
 
     min_icon = text_layer_create(
-        GRect(PBL_IF_ROUND_ELSE(60, 75), PBL_IF_ROUND_ELSE(16, -2), bounds.size.w, 50));
+        GRect(PBL_IF_ROUND_ELSE(60, 75), PBL_IF_ROUND_ELSE(18, -2), bounds.size.w, 50));
     text_layer_set_background_color(min_icon, GColorClear);
     text_layer_set_text_color(min_icon, GColorWhite);
     text_layer_set_text_alignment(min_icon, GTextAlignmentLeft);
@@ -160,15 +243,15 @@ static void watchface_load(Window *window) {
     text_layer_set_text(min_icon, "\U0000F044");
 
     temp_max = text_layer_create(
-        GRect(PBL_IF_ROUND_ELSE(105, 120), PBL_IF_ROUND_ELSE(20, 2), bounds.size.w, 50));
+        GRect(PBL_IF_ROUND_ELSE(105, 120), PBL_IF_ROUND_ELSE(22, 2), bounds.size.w, 50));
     text_layer_set_background_color(temp_max, GColorClear);
     text_layer_set_text_color(temp_max, GColorWhite);
     text_layer_set_text_alignment(temp_max, GTextAlignmentLeft);
     text_layer_set_font(temp_max, base_font);
-    text_layer_set_text(temp_max, "16");
+    text_layer_set_text(temp_max, "");
 
     max_icon = text_layer_create(
-        GRect(PBL_IF_ROUND_ELSE(95, 110), PBL_IF_ROUND_ELSE(16, -2), bounds.size.w, 50));
+        GRect(PBL_IF_ROUND_ELSE(95, 110), PBL_IF_ROUND_ELSE(18, -2), bounds.size.w, 50));
     text_layer_set_background_color(max_icon, GColorClear);
     text_layer_set_text_color(max_icon, GColorWhite);
     text_layer_set_text_alignment(max_icon, GTextAlignmentLeft);
@@ -231,6 +314,13 @@ static void watchface_unload(Window *window) {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     update_time();
+    if(tick_time->tm_min % 30 == 0) {
+        DictionaryIterator *iter;
+        app_message_outbox_begin(&iter);
+
+        dict_write_uint8(iter, 0, 0);
+        app_message_outbox_send();
+    }
 }
 
 #if defined(PBL_HEALTH)
@@ -264,9 +354,7 @@ static void update_steps_data(void) {
 
         if (mask_steps_last_week & HealthServiceAccessibilityMaskAvailable) {
             steps_last_week = (int)health_service_sum(metric_steps, start_last_week, end_last_week);
-            if (steps_last_week > current_steps) {
-                steps_or_sleep_text[0] = '-';
-            } else if (steps_last_week < current_steps) {
+            if (steps_last_week < current_steps) {
                 steps_or_sleep_text[0] = '+';
             }
         }
@@ -280,9 +368,7 @@ static void update_steps_data(void) {
         
         if (mask_dist_last_week & HealthServiceAccessibilityMaskAvailable) {
             dist_last_week = (int)health_service_sum(metric_dist, start_last_week, end_last_week);
-            if (dist_last_week > current_dist) {
-                dist_or_deep_text[0] = '-';
-            } else if (dist_last_week < current_dist) {
+            if (dist_last_week < current_dist) {
                 dist_or_deep_text[0] = '+';
             }
         }
@@ -326,6 +412,12 @@ static void init(void) {
     window_stack_push(watchface, true);
     update_time();
 
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
+    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+    
     #if defined(PBL_HEALTH)
         has_health = health_service_events_subscribe(health_handler, NULL);
         if (has_health) {
