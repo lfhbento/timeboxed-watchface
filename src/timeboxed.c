be1c4e9f41dd1562ac008a956245558430b03ca7
Luis Felipe Hussin Bento <hussin@gmail.com>
2016-02-07 12:43:16 -0800

    enable sleep settings

--- src/timeboxed.c
+++ src/timeboxed.c
@@ -54,12 +54,16 @@ static char max_text[8];
 static char min_text[8];
 static char weather_text[4];
 static char tz_text[20];
+static char s_battery_buffer[7];
 static char steps_or_sleep_text[16];
 static char dist_or_deep_text[16];
-static char s_battery_buffer[7];
 static bool has_health;
 static bool weather_enabled;
 static bool health_enabled;
+static bool sleep_data_visible;
+static bool was_asleep;
+static int woke_up_at_hour;
+static int woke_up_at_min;
 static int tz_hour;
 static int tz_minute;
 static char tz_name[TZ_LEN];
@@ -111,14 +115,8 @@ static void update_time() {
     struct tm *tick_time = localtime(&temp);
     struct tm *gmt_time = gmtime(&temp);
     char temp_hours[8];
-    snprintf(temp_hours, sizeof(temp_hours), "%d", tz_hour);
-    APP_LOG(APP_LOG_LEVEL_INFO, temp_hours);
     gmt_time->tm_hour = gmt_time->tm_hour + tz_hour;
-    snprintf(temp_hours, sizeof(temp_hours), "%d", gmt_time->tm_hour);
-    APP_LOG(APP_LOG_LEVEL_INFO, temp_hours);
     mktime(gmt_time);
-    snprintf(temp_hours, sizeof(temp_hours), "%d", gmt_time->tm_hour);
-    APP_LOG(APP_LOG_LEVEL_INFO, temp_hours);
     gmt_time->tm_min = gmt_time->tm_min + tz_minute;
     mktime(gmt_time);
     
@@ -213,6 +211,7 @@ static void update_steps_data(void) {
     uint16_t current_dist_dec = 0;
     uint16_t steps_last_week = 0;
     uint16_t dist_last_week = 0;
+    
 
     HealthServiceAccessibilityMask mask_steps =
         health_service_metric_accessible(metric_steps, start, end);
@@ -234,7 +233,8 @@ static void update_steps_data(void) {
                 steps_or_sleep_text[0] = '+';
             }
         }
-
+        
+        APP_LOG(APP_LOG_LEVEL_INFO, steps_or_sleep_text);
         text_layer_set_text(steps_or_sleep, steps_or_sleep_text);
     }
 
@@ -256,8 +256,78 @@ static void update_steps_data(void) {
             }
         }
 
+        APP_LOG(APP_LOG_LEVEL_INFO, dist_or_deep_text);
         text_layer_set_text(dist_or_deep, dist_or_deep_text);
     }
+    
+    HealthActivityMask activities = health_service_peek_current_activities();
+    bool is_sleeping = activities & HealthActivitySleep || activities & HealthActivityRestfulSleep;
+    if (is_sleeping) {
+        APP_LOG(APP_LOG_LEVEL_INFO, "we are asleep!");
+        text_layer_set_text(steps_or_sleep, "zzz");
+        text_layer_set_text(dist_or_deep, "zzz");
+    }
+}
+
+static void update_sleep_data(void) {
+    HealthMetric metric_sleep = HealthMetricSleepSeconds;
+    HealthMetric metric_deep = HealthMetricSleepRestfulSeconds;
+    
+    time_t start = time_start_of_today() - (SECONDS_PER_HOUR * 4); // 8pm
+    time_t end = time(NULL);
+    time_t start_last_week = start - (SECONDS_PER_HOUR * 4) - (SECONDS_PER_HOUR * HOURS_PER_DAY * 7); // last week 8pm
+    time_t end_last_week = end - (SECONDS_PER_HOUR * HOURS_PER_DAY * 7);
+
+    uint16_t current_sleep = 0;
+    uint16_t current_deep = 0;
+    uint16_t sleep_last_week = 0;
+    uint16_t deep_last_week = 0;
+    
+    HealthServiceAccessibilityMask mask_sleep =
+        health_service_metric_accessible(metric_sleep, start, end);
+    HealthServiceAccessibilityMask mask_sleep_last_week =
+        health_service_metric_accessible(metric_sleep, start_last_week, end_last_week);
+    HealthServiceAccessibilityMask mask_deep =
+        health_service_metric_accessible(metric_deep, start, end);
+    HealthServiceAccessibilityMask mask_deep_last_week =
+        health_service_metric_accessible(metric_deep, start_last_week, end_last_week);
+
+    if (mask_sleep & HealthServiceAccessibilityMaskAvailable) {
+        current_sleep = (int)health_service_sum(metric_sleep, start, end);
+        int current_sleep_hours = current_sleep / SECONDS_PER_HOUR;
+        int current_sleep_minutes = (current_sleep - (current_sleep_hours * SECONDS_PER_HOUR))/SECONDS_PER_MINUTE;
+        
+        snprintf(steps_or_sleep_text, sizeof(steps_or_sleep_text), " %dh%dm", current_sleep_hours, current_sleep_minutes);
+        
+        if (mask_sleep_last_week & HealthServiceAccessibilityMaskAvailable) {
+            sleep_last_week = (int)health_service_sum(metric_sleep, start_last_week, end_last_week);
+            if (sleep_last_week < current_sleep) {
+                steps_or_sleep_text[0] = '+';
+            }
+        }
+        
+        APP_LOG(APP_LOG_LEVEL_INFO, steps_or_sleep_text);
+        text_layer_set_text(steps_or_sleep, steps_or_sleep_text);
+    }
+
+    if (mask_deep & HealthServiceAccessibilityMaskAvailable) {
+        current_deep = (int)health_service_sum(metric_deep, start, end);
+        int current_deep_hours = current_deep / SECONDS_PER_HOUR;
+        int current_deep_minutes = (current_deep - (current_deep_hours * SECONDS_PER_HOUR))/SECONDS_PER_MINUTE;
+        
+        snprintf(dist_or_deep_text, sizeof(dist_or_deep_text), " %dh%dm", current_deep_hours, current_deep_minutes);
+        
+        if (mask_deep_last_week & HealthServiceAccessibilityMaskAvailable) {
+            deep_last_week = (int)health_service_sum(metric_deep, start_last_week, end_last_week);
+            if (deep_last_week < current_deep) {
+                dist_or_deep_text[0] = '+';
+            }
+        }
+        
+        APP_LOG(APP_LOG_LEVEL_INFO, dist_or_deep_text);
+        text_layer_set_text(dist_or_deep, dist_or_deep_text);
+    }
+
 }
 
 static void health_handler(HealthEventType event, void *context) {
@@ -267,17 +337,19 @@ static void health_handler(HealthEventType event, void *context) {
                 "New HealthService HealthEventSignificantUpdate event");
             break;
         case HealthEventMovementUpdate:
-            update_steps_data();
+            if (!sleep_data_visible) {
+                update_steps_data();
+            }
             break;
         case HealthEventSleepUpdate:
-            APP_LOG(APP_LOG_LEVEL_INFO, 
-                "New HealthService HealthEventSleepUpdate event");
+            if (sleep_data_visible) {
+                update_sleep_data();
+            }
             break;
     }
 }
 
 #endif
-
 
 static void toggle_health(void) {
     has_health = false;
@@ -286,7 +358,7 @@ static void toggle_health(void) {
         health_enabled = persist_read_int(KEY_ENABLEHEALTH);
         if (health_enabled) {
             has_health = health_service_events_subscribe(health_handler, NULL);
-            if (has_health) {
+            if (has_health && !sleep_data_visible) {
                 update_steps_data();
             }
         } else {
@@ -320,10 +392,39 @@ static void update_weather(void) {
     app_message_outbox_send();
 }
 
+static void update_weather_values(int temp_val, int max_val, int min_val, int weather_val) {
+    bool useCelsius = persist_exists(KEY_USECELSIUS) && persist_read_int(KEY_USECELSIUS);
+        
+    snprintf(temp_text, sizeof(temp_text), (useCelsius ? "%dc" : "%df"), temp_val);
+    snprintf(max_text, sizeof(max_text), "%d", max_val);
+    snprintf(min_text, sizeof(min_text), "%d", min_val);
+    snprintf(weather_text, sizeof(weather_text), "%s", weather_conditions[weather_val]);
+
+    persist_write_int(KEY_TEMP, temp_val);
+    persist_write_int(KEY_MAX, max_val);
+    persist_write_int(KEY_MIN, min_val);
+    persist_write_int(KEY_WEATHER, weather_val);
+
+    text_layer_set_text(temp_cur, temp_text);
+    text_layer_set_text(temp_max, max_text);
+    text_layer_set_text(temp_min, min_text);
+    text_layer_set_text(weather, weather_text);
+    text_layer_set_text(min_icon, "\U0000F044");
+    text_layer_set_text(max_icon, "\U0000F058");
+}
+
 static void toggle_weather(void) {
     weather_enabled = persist_exists(KEY_ENABLEWEATHER) && persist_read_int(KEY_ENABLEWEATHER);
     if (weather_enabled) {
         APP_LOG(APP_LOG_LEVEL_INFO, "weather enabled");
+        if (persist_exists(KEY_TEMP) && persist_exists(KEY_MAX) && persist_exists(KEY_MIN) && persist_exists(KEY_WEATHER)) {
+            int temp_val = persist_read_int(KEY_TEMP);
+            int max_val = persist_read_int(KEY_MAX);
+            int min_val = persist_read_int(KEY_MIN);
+            int weather_val = persist_read_int(KEY_WEATHER);
+
+            update_weather_values(temp_val, max_val, min_val, weather_val);
+        }
         update_weather();
     } else {
         APP_LOG(APP_LOG_LEVEL_INFO, "weather disabled");
@@ -343,18 +444,12 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
     Tuple *weather_tuple = dict_find(iterator, KEY_WEATHER);
 
     if(temp_tuple && max_tuple && min_tuple && weather_tuple && weather_enabled) {
-        bool useCelsius = persist_exists(KEY_USECELSIUS) && persist_read_int(KEY_USECELSIUS);
-        snprintf(temp_text, sizeof(temp_text), (useCelsius ? "%dc" : "%df"), (int)temp_tuple->value->int32);
-        snprintf(max_text, sizeof(max_text), "%d", (int)max_tuple->value->int32);
-        snprintf(min_text, sizeof(min_text), "%d", (int)min_tuple->value->int32);
-        snprintf(weather_text, sizeof(weather_text), "%s", weather_conditions[(int)weather_tuple->value->int32]);
+        int temp_val = (int)temp_tuple->value->int32;
+        int max_val = (int)max_tuple->value->int32;
+        int min_val = (int)min_tuple->value->int32;
+        int weather_val = (int)weather_tuple->value->int32;
 
-        text_layer_set_text(temp_cur, temp_text);
-        text_layer_set_text(temp_max, max_text);
-        text_layer_set_text(temp_min, min_text);
-        text_layer_set_text(weather, weather_text);
-        text_layer_set_text(min_icon, "\U0000F044");
-        text_layer_set_text(max_icon, "\U0000F058");
+        update_weather_values(temp_val, max_val, min_val, weather_val);
 
         APP_LOG(APP_LOG_LEVEL_INFO, "Weather updated!");
         return;
@@ -523,7 +618,6 @@ static void battery_handler(BatteryChargeState charge_state) {
         snprintf(s_battery_buffer, sizeof(s_battery_buffer), (charge_state.charge_percent <= 20 ? "! %d%%" : "%d%%"), charge_state.charge_percent);
     }
 
-
     text_layer_set_text(battery, s_battery_buffer);
 }
 
@@ -668,7 +762,35 @@ static void watchface_unload(Window *window) {
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
     update_time();
-    if(tick_time->tm_min % 30 == 0 && weather_enabled) {
+    bool is_sleeping = false;
+
+    #if defined(PBL_HEALTH)
+        if (health_enabled) {
+            HealthActivityMask activities = health_service_peek_current_activities();
+            is_sleeping = activities & HealthActivitySleep || activities & HealthActivityRestfulSleep;
+        
+    
+            bool sleep_data_enabled = persist_exists(KEY_SHOWSLEEP) && persist_read_int(KEY_SHOWSLEEP);
+            
+            if (!is_sleeping && was_asleep && sleep_data_enabled) {
+                APP_LOG(APP_LOG_LEVEL_INFO, "We woke up!");
+                sleep_data_visible = true;
+                woke_up_at_hour = tick_time->tm_hour;
+                woke_up_at_min = tick_time->tm_min;
+                was_asleep = false;
+                update_sleep_data();
+            }
+
+            if (sleep_data_visible && tick_time->tm_hour >= woke_up_at_hour + 1 && tick_time->tm_min >= woke_up_at_min) {
+                APP_LOG(APP_LOG_LEVEL_INFO, "Past an hour after wake up!");
+                sleep_data_visible = false;
+            }
+        }
+    #endif
+
+    uint16_t tick_interval = is_sleeping ? 59 : 30;
+
+    if(tick_time->tm_min % tick_interval == 0 && weather_enabled) {
         update_weather();
     }
 }
@@ -677,6 +799,8 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void init(void) {
     tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
+    was_asleep = false;
+    sleep_data_visible = false;
 
     watchface = window_create();
 
