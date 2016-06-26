81b93bfe06fd22c1e47dca87a72f3a01b77dd599
Luis Felipe Hussin Bento <hussin@gmail.com>
2016-06-26 15:50:13 -0700

    adjusted wind speed position and removed configs

--- src/text.c
+++ src/text.c
@@ -160,9 +160,9 @@ void create_text_layers(Window* window) {
 
     int wind_slot = get_slot_for_module(MODULE_WIND);
     GPoint speed_pos = get_pos_for_item(wind_slot, SPEED_ITEM, mode, selected_font);
-    speed = text_layer_create(GRect(speed_pos.x, speed_pos.y, width, 50));
+    speed = text_layer_create(GRect(speed_pos.x, speed_pos.y, PBL_IF_ROUND_ELSE(width, 42), 50));
     text_layer_set_background_color(speed, GColorClear);
-    text_layer_set_text_alignment(speed, GTextAlignmentLeft);
+    text_layer_set_text_alignment(speed, PBL_IF_ROUND_ELSE(GTextAlignmentLeft, GTextAlignmentCenter));
 
     GPoint direction_pos = get_pos_for_item(wind_slot, DIRECTION_ITEM, mode, selected_font);
     direction = text_layer_create(GRect(direction_pos.x, direction_pos.y, width, 50));
